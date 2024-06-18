b2b709094b38a7e9c5d80805729219f3468a5fd8
Gustavo Adolfo Muñoz Reyes <157825951+Desarrollo8Tuatara@users.noreply.github.com>
2024-06-17 19:55:18 -0500

    Ajustes de responsive

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CC7N52NR.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-CG44W2YI.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
