da231bf4a88ae6d0dcf483e2c7ab4910e918e996
Gustavo Adolfo Muñoz Reyes <157825951+Desarrollo8Tuatara@users.noreply.github.com>
2024-06-17 19:49:58 -0500

    Ajustes visuales

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TLCLA5ES.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-CC7N52NR.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
