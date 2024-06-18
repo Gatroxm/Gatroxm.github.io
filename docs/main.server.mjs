0891e693964eafb1336512ea9d48e1e6e326defd
Gustavo Adolfo Muñoz Reyes <157825951+Desarrollo8Tuatara@users.noreply.github.com>
2024-06-17 20:28:32 -0500

    Ajustes de link en el footer

--- docs/main.server.mjs
+++ docs/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-KZTF3YIL.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-3F6ZCWKC.mjs";import"./chunk-HUP77I7O.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
